332f8e63663f94b9d1e4a5737b7b49f7df818daa
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-07-08 11:51:19 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -9,7 +9,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 from AnonXMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from AnonXMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
